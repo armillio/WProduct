264bcd316909c3bf4321f522bfc12e388ea87414
Armando <armando@happy.computer>
2019-10-24 17:34:05 -0500

    Initial commit

---
+++ WProducts/WProducts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WProducts
+//
+//  Created by Armando on 10/24/19.
+//  Copyright © 2019 Happy Computer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

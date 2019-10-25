bdc9cd1916bcdf1daeed6ad4040c94534e2aface
Armando <armando@happy.computer>
2019-10-25 11:28:31 -0500

    Added extension to register collection view nibs

---
+++ WProducts/WProducts/UI/Extensions/UICollectionView+RegisterNib.swift
@@ -0,0 +1,23 @@
+
+import UIKit
+
+protocol UICollectionViewCellStaticProtocol {
+    static func cellIdentifier() -> String
+    //static func cellHeight(w) -> CGFloat
+}
+
+extension UICollectionViewCellStaticProtocol {
+    static func cellIdentifier() -> String {
+        return String(describing: type(of: self))
+    }
+}
+
+extension UICollectionView {
+    func registerNib<T:UICollectionViewCell>(_ classValue: T.Type) where T:UICollectionViewCellStaticProtocol {
+        register(UINib(nibName: String(describing: classValue), bundle: nil), forCellWithReuseIdentifier: classValue.cellIdentifier())
+    }
+    
+    func registerHeaderNib<T:UICollectionViewCell>(_ classValue: T.Type) where T:UICollectionViewCellStaticProtocol {
+        register(UINib(nibName: String(describing: classValue), bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: classValue.cellIdentifier())
+    }
+}

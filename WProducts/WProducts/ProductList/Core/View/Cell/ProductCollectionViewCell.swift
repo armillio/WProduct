5aa63eaf4766b3963d26f88dfb59a3418d37ecd3
Armando <armando@happy.computer>
2019-10-25 11:47:54 -0500

    Product List collection view cell added

---
+++ WProducts/WProducts/ProductList/Core/View/Cell/ProductCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCollectionViewCell.swift
+//  WProducts
+//
+//  Created by Armando on 10/24/19.
+//  Copyright © 2019 Happy Computer. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell, UICollectionViewCellStaticProtocol {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

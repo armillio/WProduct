a04047175ddb5a3ea8884b542ab23cb4958d012d
Armando <armando@happy.computer>
2019-10-25 17:42:02 -0500

    Tweaked the design of the cell

--- WProducts/WProducts/ProductList/Core/View/Cell/ProductCollectionViewCell.swift
+++ WProducts/WProducts/ProductList/Core/View/Cell/ProductCollectionViewCell.swift
@@ -5,8 +5,8 @@ class ProductCollectionViewCell: UICollectionViewCell, UICollectionViewCellStati
     
     @IBOutlet weak var productImage: UIImageView!
     @IBOutlet weak var productName: UILabel!
-    @IBOutlet weak var reviewRating: UILabel!
     @IBOutlet weak var reviewCount: UILabel!
+    @IBOutlet weak var reviewRating: CosmosView!
     @IBOutlet weak var productPrice: UILabel!
         
     override func awakeFromNib() {
@@ -17,29 +17,31 @@ class ProductCollectionViewCell: UICollectionViewCell, UICollectionViewCellStati
         guard let viewWidth = contentWidth else { return 0.0 }
         let contentViewWidth = viewWidth - (100.0 + 16.0 + 8.0 + 16.0)
         
-        let productName = self.productName.determineLabelHeight(withWidth: contentViewWidth, withFont: UIFont.preferredFont(forTextStyle: .body), withText: product?.name)
+        let label = UILabel.init()
         
-        let reviewCount = self.reviewCount.determineLabelHeight(withWidth: contentViewWidth, withFont: UIFont.preferredFont(forTextStyle: .caption2), withText: String.init(format: "%d", product?.reviewCount ?? 9999.99))
+        let productName = label.determineLabelHeight(withWidth: contentViewWidth, withFont: UIFont.preferredFont(forTextStyle: .footnote), withText: product?.name)
         
-        let productPrice = self.productPrice.determineLabelHeight(withWidth: contentViewWidth, withFont: UIFont.preferredFont(forTextStyle: .footnote), withText: product?.price)
+        let reviewCount = label.determineLabelHeight(withWidth: contentViewWidth, withFont: UIFont.preferredFont(forTextStyle: .caption2), withText: String.init(format: "%ld", product?.reviewCount ?? 9999.99))
         
-        let padding:CGFloat = 16.0 + 8.0 + 8.0
+        let productPrice = label.determineLabelHeight(withWidth: contentViewWidth, withFont: UIFont.preferredFont(forTextStyle: .title2), withText: product?.price)
+        
+        let padding:CGFloat = 24.0 + 8.0 + 8.0
         var calculatedHeight = productName + reviewCount + productPrice + padding
         
-        if calculatedHeight < 100 {
-            calculatedHeight = 100
+        if calculatedHeight < (100 + 16) {
+            calculatedHeight = 116
         }
         
-        return calculatedHeight  + 16.0
+        return calculatedHeight  + 16.0 + 1.0
     }
     
     func configureCell(withProduct product: ProductViewModel){
-        self.productName.text = product.name
-        self.reviewCount.text = String.init(format: "%d", product.reviewCount)
+        self.productName.text = product.name.trimmingCharacters(in: NSCharacterSet.whitespaces)
+        self.productName.adjustHeightOfLabel()
+        self.reviewCount.text = String.init(format: "(%d)", product.reviewCount ?? 0)
         self.productPrice.text = product.price
-        self.reviewRating.text = String.init(format: "%d", product.reviewRating)
+        self.reviewRating.rating = product.reviewRating ?? 0
         let url = URL.init(string: String.init(format:"%@%@", ApplicationConstants.APIBaseURL, product.image ?? "/"))
-
         if let imageURL = url {
             self.productImage.download(fromURL: imageURL)
         }

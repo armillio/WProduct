2cadcf2542098ca99cf4eef9aacfea50999ac6f4
Armando <armando@happy.computer>
2019-10-26 21:08:34 -0500

    Extension to convert html to an Attributed String

---
+++ WProducts/WProducts/UI/Extensions/String+Extended.swift
@@ -0,0 +1,13 @@
+
+import Foundation
+
+extension String {
+    var html: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return NSAttributedString() }
+        do {
+            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
+            return NSAttributedString()
+        }
+    }
+}

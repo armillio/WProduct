84b5e0288316deddf741ad50c3361667bef2d2b0
Armando <armando@happy.computer>
2019-10-27 21:27:39 -0500

    Added method to detect side swipe

--- WProducts/WProducts/UI/Extensions/UIScrollViewDelegate+DragDownFromBottom.swift
+++ WProducts/WProducts/UI/Extensions/UIScrollViewDelegate+DragDownFromBottom.swift
@@ -7,4 +7,10 @@ extension UIScrollViewDelegate {
         let maximumOffset = scrollView.contentSize.height - scrollView.frame.size.height - delta
         return (currentOffset - maximumOffset) > 0 && maximumOffset > 0
     }
+    
+    func scrollViewDidDragLeftFromSide(_ scrollView: UIScrollView, delta: CGFloat = 0.0) -> Bool {
+        let currentOffset = scrollView.contentOffset.x - delta
+        let maximumOffset = scrollView.contentSize.width - scrollView.frame.size.width - delta
+        return (currentOffset - maximumOffset) > 0 && maximumOffset > 0
+    }
 }

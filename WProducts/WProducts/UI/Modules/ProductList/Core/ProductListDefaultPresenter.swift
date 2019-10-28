901248a7101c1932ec3fe2a7539b7809b71a9bcc
Armando <armando@happy.computer>
2019-10-28 17:25:24 -0500

    Uncommented code I was testing

--- WProducts/WProducts/UI/Modules/ProductList/Core/ProductListDefaultPresenter.swift
+++ WProducts/WProducts/UI/Modules/ProductList/Core/ProductListDefaultPresenter.swift
@@ -30,17 +30,16 @@ class ProductListDefaultPresenter: ProductListPresenter {
             self.view?.displayActivityIndicator()
         }
         interactorManager.getProductListData(withPage: 1, pageSize: 20) { (products, error) in
-            self.view?.displayEmptyScreen(withText: "ERROR synchronizing with server")
-//            if error != nil {
-//                self.view?.displayEmptyScreen(withText: "ERROR synchronizing with server")
-//            } else {
-//                self.currentPage = 1
-//                if let products = products {
-//                    let viewModel = self.viewModelBuilder.buildViewModel(withProducts: products)
-//                    self.viewModel = viewModel
-//                    self.view?.displayProductList(viewModel)
-//                }
-//            }
+            if error != nil {
+                self.view?.displayEmptyScreen(withText: "ERROR synchronizing with server")
+            } else {
+                self.currentPage = 1
+                if let products = products {
+                    let viewModel = self.viewModelBuilder.buildViewModel(withProducts: products)
+                    self.viewModel = viewModel
+                    self.view?.displayProductList(viewModel)
+                }
+            }
         }
     }
     
